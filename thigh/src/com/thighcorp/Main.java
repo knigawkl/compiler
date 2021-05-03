2a2ea7e6a6a8eeb1e765e7cc5c04dd4f13ab0404
Łukasz Knigawka <lukasz.knigawka@outlook.com>
2021-05-03 13:22:52 +0200

    Added antlr imports

--- thigh/src/com/thighcorp/Main.java
+++ thigh/src/com/thighcorp/Main.java
@@ -1,5 +1,10 @@
 package com.thighcorp;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 public class Main {
 
     public static void main(String[] args) {
