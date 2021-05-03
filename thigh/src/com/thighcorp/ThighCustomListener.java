07ffe9da29fc75787520e4da0f764d2503ed9a35
Łukasz Knigawka <lukasz.knigawka@outlook.com>
2021-05-03 21:01:59 +0200

    cleanup

--- thigh/src/com/thighcorp/ThighCustomListener.java
+++ thigh/src/com/thighcorp/ThighCustomListener.java
@@ -1,18 +1,9 @@
 package com.thighcorp;
 
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.ErrorNode;
-import org.antlr.v4.runtime.tree.TerminalNode;
-
 import java.util.HashMap;
 
 public class ThighCustomListener extends ThighBaseListener {
     HashMap<String, String> variableMap = new HashMap();
-
-    @Override
-    public void enterProgram(ThighParser.ProgramContext ctx) {
-
-    }
 
     @Override
     public void exitProgram(ThighParser.ProgramContext ctx) {
@@ -20,17 +11,7 @@ public class ThighCustomListener extends ThighBaseListener {
     }
 
     @Override
-    public void enterStatement(ThighParser.StatementContext ctx) {
-
-    }
-
-    @Override
     public void exitStatement(ThighParser.StatementContext ctx) {
-
-    }
-
-    @Override
-    public void enterPrint_statement(ThighParser.Print_statementContext ctx) {
 
     }
 
@@ -45,17 +26,7 @@ public class ThighCustomListener extends ThighBaseListener {
     }
 
     @Override
-    public void enterRead_statement(ThighParser.Read_statementContext ctx) {
-
-    }
-
-    @Override
     public void exitRead_statement(ThighParser.Read_statementContext ctx) {
-
-    }
-
-    @Override
-    public void enterAssign_statement(ThighParser.Assign_statementContext ctx) {
 
     }
 
@@ -65,36 +36,25 @@ public class ThighCustomListener extends ThighBaseListener {
         ThighParser.ValueContext a = ctx.assign_value().value();
         ThighParser.Arithmetic_operationContext b = ctx.assign_value().arithmetic_operation();
 
-        if (a != null){
-            if (a.INT() != null){
+        if (a != null) {
+            if (a.INT() != null) {
                 this.variableMap.put(ctx.ID().getText(),
                         a.INT().getText());
-            }else if (a.STRING() != null){
+            } else if (a.STRING() != null) {
                 this.variableMap.put(ctx.ID().getText(),
                         a.STRING().getText());
-
-            }else if (a.REAL() != null){
+            } else if (a.REAL() != null) {
                 this.variableMap.put(ctx.ID().getText(),
                         a.REAL().getText());
             }
-        }else if (b != null){
+        } else if (b != null) {
             this.variableMap.put(ctx.ID().getText(),
                     b.getText());
         }
     }
 
     @Override
-    public void enterFunction_definition(ThighParser.Function_definitionContext ctx) {
-
-    }
-
-    @Override
     public void exitFunction_definition(ThighParser.Function_definitionContext ctx) {
-
-    }
-
-    @Override
-    public void enterFunction_body(ThighParser.Function_bodyContext ctx) {
 
     }
 
@@ -104,28 +64,12 @@ public class ThighCustomListener extends ThighBaseListener {
     }
 
     @Override
-    public void enterExpression(ThighParser.ExpressionContext ctx) {
-
-    }
-
-    @Override
     public void exitExpression(ThighParser.ExpressionContext ctx) {
 
     }
 
     @Override
-    public void enterArithmetic_operation(ThighParser.Arithmetic_operationContext ctx) {
-
-    }
-
-    @Override
     public void exitArithmetic_operation(ThighParser.Arithmetic_operationContext ctx) {
-
-    }
-
-
-    @Override
-    public void enterArithmetic_operator(ThighParser.Arithmetic_operatorContext ctx) {
 
     }
 
@@ -135,32 +79,7 @@ public class ThighCustomListener extends ThighBaseListener {
     }
 
     @Override
-    public void enterValue(ThighParser.ValueContext ctx) {
-
-    }
-
-    @Override
     public void exitValue(ThighParser.ValueContext ctx) {
-
-    }
-
-    @Override
-    public void visitTerminal(TerminalNode terminalNode) {
-
-    }
-
-    @Override
-    public void visitErrorNode(ErrorNode errorNode) {
-
-    }
-
-    @Override
-    public void enterEveryRule(ParserRuleContext parserRuleContext) {
-
-    }
-
-    @Override
-    public void exitEveryRule(ParserRuleContext parserRuleContext) {
 
     }
 }
